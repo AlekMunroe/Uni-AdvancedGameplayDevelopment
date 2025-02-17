0e8fa2750de3488759ca962586ee8aafc707b462
Bomet1837 <158185256+Bomet1837@users.noreply.github.com>
2025-02-17 16:00:22 +0000

    Starting combo lock interface design + programming

---
+++ Assets/Scripts/UI/Interactables/ComboLockController.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComboLockController : MonoBehaviour
+{
+    public int[] combination;
+    public int corectCombination;
+    
+    public TMPro.TextMeshProUGUI[] combinationTexts;
+}
